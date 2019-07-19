33fe5ba08118993d926960f53c9e45b67bc75ea2
Chris Seals <seals_christopher@bah.com>
2019-07-19 09:34:06 -0500

    add prediction panel, start forecasting lines

--- dashboard/Keg_Dashboard.py
+++ dashboard/Keg_Dashboard.py
@@ -6,9 +6,30 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
+import pandas as pd
 import plotly.graph_objs as go
+from sklearn.model_selection import train_test_split
+# import pymysql
+from statsmodels.tsa.arima_model import ARIMA
 
+
+def arima_kegs(times, keg_data):
+    keg_df = pd.DataFrame({'Time': times, 'keg_data': keg_data})
+    keg_train, keg_test = train_test_split(keg_df.keg_data, test_size=0.25, random_state=42, shuffle=False)
+    for k, v in data_dict.items():
+        if v == keg_data:
+            name = f'{k} Forecast'
+        else:
+            name = 'forecasted'
+    model = ARIMA(keg_train, order=(3, 2, 1))
+    fitted = model.fit(disp=-1)
+
+    # Forecast
+    forecast, se, conf = fitted.forecast(len(keg_test), alpha=0.05)  # 95% conf
+    #return keg_test.index, forecast
+    return {'x':keg_test.index, 'y': forecast, 'type': 'line', 'name':name}
+
+# {'x': times, 'y': list(v), 'type': 'line', 'name': k} for k, v in data_dict.items()
 app = dash.Dash('Venture Cafe Beverage Forecasting')
 
 app_color = {"graph_bg": "#082255",
@@ -38,9 +59,7 @@ hour = now.hour
 
 totalTime = (hour * 3600) + (minute * 60) + (sec)
 
-
-# TODO ### Shrink Jumbotron or change to normal header
-# TODO ### Add Prediction Panel
+# TODO ### Fix lines for forecasting so they go into the future, not overlaying the last several steps
 # TODO ### Make colors match for all graphs
 
 start_time = time.time()
@@ -69,7 +88,7 @@ def update_keg_values(times, keg_1, keg_2, keg_3, keg_4):
     else:
         for data_of_interest in [keg_1, keg_2, keg_3, keg_4]:
             if random.random() < 0.5:
-                data_of_interest.append(data_of_interest[-1]-random.random()*8)
+                data_of_interest.append(data_of_interest[-1]-random.random()*1)
             else:
                 data_of_interest.append(data_of_interest[-1])
 
@@ -138,7 +157,7 @@ app.layout = html.Div(
 
             dcc.Interval(
                 id='graph-update',
-                interval=10000,
+                interval=1000,          ### Original 10000
                 n_intervals=10000       ### Added to get around events
             )
 
@@ -228,30 +247,44 @@ def update_graphs(n_intervals):
     )
 
     # UPDATE PREDICTIONS GRAPH
-    fig3 = html.Div(
-        dcc.Graph(px.scatter
-            # id='pred-lines',  # data_name,
-            # # animate=True,
-            # figure={'data': [
-            #
-            #     {'x': [keg_1_name, keg_2_name, keg_3_name, keg_4_name],
-            #      'y': [60 - keg_1[-1], 60 - keg_2[-1], 60 - keg_3[-1], 60 - keg_4[-1]],
-            #      'type': 'line'}
-            #
-            # ],
-            #     'layout': go.Layout(
-            #         xaxis=dict(
-            #             # range=[min(times),max(times)],
-            #             title='Quantities Dispensed'
-            #         ),  # max(times)
-            #         yaxis=dict(
-            #             range=[0, 60],  # range=[min(data_dict[data_name])-10,131],
-            #             title='Weight Dispensed (lbs)'),
-            #         margin={'l': 75, 'r': 75, 't': 45, 'b': 85},  # margins for top and left for text
-            #         title='{}'.format('Beer Dispensed to Guests (lbs)'),
-            #         height=400,
-            #     )
-            # }
+    fig3 = html.Div(  # graphs.append(html.Div(
+        dcc.Graph(
+            id='prediction-lines',  # data_name,
+            animate=True,
+            figure={'data': [*[
+                # data_dict#[data]
+
+                {'x': times, 'y': list(v), 'type': 'line', 'name': k} for k, v in data_dict.items()],
+                *[arima_kegs(times, v) for k, v in data_dict.items()if len(times)>10]
+                                                                                  # removed x:times
+            ],
+
+                'layout': go.Layout(
+                    legend=dict(
+                        orientation='v',
+                        # yanchor='top',
+                        # xanchor='right',
+                        y=.01,
+                        x=0,
+                        font=dict(
+                            family="sans-serif",
+                            size=16,
+                            color="black"
+                        )
+                    ),
+                    xaxis=dict(
+                        range=[min(times), max(times) + 10],
+                        title='Time Elapsed (sec)'
+                    ),  # max(times)
+                    yaxis=dict(
+                        range=[0, 60],  # range=[min(data_dict[data_name])-10,131],
+                        title='Weight of Remaining Beverage (lbs)'),
+                    margin={'l': 75, 'r': 75, 't': 45, 'b': 45},  # margins for top and left for text
+                    title='{}'.format('Keg quantities remaining (actual)'),
+
+                    height=700
+                )
+            }
         )  # , className='eight col')
     )
     return fig, fig2, fig3
